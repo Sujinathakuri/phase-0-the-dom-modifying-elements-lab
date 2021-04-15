139947cd5d8734c70afbe7130ac4a65d4d5f4ccc
Sujinathakuri <sujchand2@gmail.com>
2021-04-15 09:30:45 +0545

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const header = document.getElementById('header');
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>"; <
+div id = "header" >
+    <
+    h1 > Poodles! < /h1> <
+    h3 > An Essay into the Pom - Pom as Aesthetic Reconfiguration of the Other from a post - Frankfurt School Appropriationist Perspective < /h3> <
+    p > < em > By: Byron Q.Poodle, Esq., BA. < /em></p >
+    <
+    /div>
