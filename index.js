69b5c9169068002ee2b9e995fa312b584084f156
Sujinathakuri <sujchand2@gmail.com>
2021-05-20 21:07:11 -0400

    commit tree a9dc1a4eef2c990fbf26aaf219d4da5dd203754f

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Write your code here!
 
-const header = document.getElementById('header');
-header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>"; <
-div id = "header" >
-    <
-    h1 > Poodles! < /h1> <
-    h3 > An Essay into the Pom - Pom as Aesthetic Reconfiguration of the Other from a post - Frankfurt School Appropriationist Perspective < /h3> <
-    p > < em > By: Byron Q.Poodle, Esq., BA. < /em></p >
-    <
-    /div>
+var el = document.getElementById("main")
+el.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
